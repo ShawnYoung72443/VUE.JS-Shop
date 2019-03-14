4769a2af2e302d85a91e35bfa4d6b4c35db9271e
Shawn Young <sryoung72443@gmail.com>
2019-03-14 02:19:34 -0400

    fixed product price and cart math

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,7 +2,7 @@
     <div class="card my-5">
         <div class="card-body">
             <h5 class="text-center card-title">{{ product.title }}</h5>
-            <p class="text-center text-muted card-text display-4">$ {{ Number(product.price).toFixed() }}</p>
+            <p class="text-center text-muted card-text display-4">$ {{ Number(product.price).toFixed(2) }}</p>
             <button :disabled="isInCart" @click="$emit('add-to-cart', product)" class="btn btn-primary form-control"> {{ isInCart ? 'Added to cart' : 'Add to cart' }} </button>
         </div>
     </div>
